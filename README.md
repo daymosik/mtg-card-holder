853253ed9bc7e001a11bb59cff5041bb87b1ee0f
Damian Majsner <d.majsner@gmail.com>
2018-07-16 18:20:25 +0200

    webpack optimization

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 [![codebeat badge](https://codebeat.co/badges/e656c153-112e-4c47-b9c9-9773004bbd30)](https://codebeat.co/projects/github-com-daymosik-mtg-card-holder-master)
 [![BCH compliance](https://bettercodehub.com/edge/badge/daymosik/mtg-card-holder?branch=master)](https://bettercodehub.com/)
 
+Latest version available at: https://mtg-card-holder.firebaseapp.com
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,41 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  }),
+  new ExtractTextPlugin('styles.css'),
+  new webpack.DefinePlugin({
+    'process.env': {
+      FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+      FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+    }
+  }),
+]
+
+const rules = [
+  { test: /\.tsx?$/, loader: 'ts-loader' },
+  {
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: 'css-loader',
+    })
+  }, {
+    test: /\.scss$/,
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: ['css-loader', 'sass-loader']
+    })
+  },
+  { test: /\.(png|jpg)$/, loader: 'file-loader' },
+]
 
 module.exports = {
   mode: process.env.NODE_ENV,
@@ -18,46 +52,7 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js']
   },
   module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        })
-      }, {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
-      { test: /\.(png|jpg)$/, loader: 'file-loader'},
-    ]
+    rules: rules,
   },
-  externals: [{
-    xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
-  }],
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-    new ExtractTextPlugin('styles.css'),
-    new webpack.DefinePlugin({
-      'process.env': {
-        FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
-        FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-      }
-    }),
-  ],
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin()
-    ],
-  },
+  plugins: plugins,
 }
