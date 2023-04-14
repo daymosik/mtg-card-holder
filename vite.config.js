72a6a1258d58af517f2dd562775e085cf5daa7b0
Damian Majsner <d.majsner@gmail.com>
2023-04-14 14:52:07 +0200

    fix watch

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import preact from '@preact/preset-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [preact(), tsconfigPaths()],
   root: 'src',
   build: {
     outDir: '../dist',
-    emptyOutDir: true,
+    emptyOutDir: false,
+  },
+  plugins: [preact(), tsconfigPaths(), splitVendorChunkPlugin()],
+  watch: {
+    chokidar: {
+      // paths: 'src/**',
+      usePolling: true,
+    },
   },
 })
